3b5aa4be60a2f2847b77abb25f24ec9475e8351b
Joni Hiltunen <joni@jonihiltunen.com>
2024-04-25 06:30:09 +0300

    Update with delta time

--- test-project.lisp
+++ test-project.lisp
@@ -11,8 +11,14 @@
    (key-down :initform nil)))
 
 (defparameter *keysdown* (make-instance 'interesting-keys))
-(defparameter *x* 100)
-(defparameter *y* 100)
+(defparameter *x* 100.0)
+(defparameter *y* 100.0)
+
+(defparameter vx 150.0)
+(defparameter vy 150.0)
+
+(defparameter *current-time* 0)
+(defparameter *prev-time* 0)
 
 (defun handle-key (keysym updown)
   (let ((keyname (case (sdl2:scancode keysym)
@@ -24,13 +30,20 @@
     (when keyname
       (setf (slot-value *keysdown* keyname) (eq :down updown)))))
 
-(defun update-world ()
-  (when (slot-value *keysdown* 'key-right) (setf *x* (+ *x* 1)))
-  (when (slot-value *keysdown* 'key-left) (setf *x* (+ *x* -1)))
-  (when (slot-value *keysdown* 'key-up) (setf *y* (+ *y* -1)))
-  (when (slot-value *keysdown* 'key-down) (setf *y* (+ *y* 1))))
+(defun update-world (dt)
+  (when (slot-value *keysdown* 'key-right) (incf *x* (* vx dt)))
+  (when (slot-value *keysdown* 'key-left) (incf *x* (* vx dt -1)))
+  (when (slot-value *keysdown* 'key-up) (incf *y* (* vy dt -1)))
+  (when (slot-value *keysdown* 'key-down) (incf *y* (* vy dt))))
+
+(defun render-all (renderer)
+  (sdl2:set-render-draw-color renderer 0 0 0 255)
+  (sdl2:render-clear renderer)
+  (sdl2:set-render-draw-color renderer 100 200 50 255)
+  (sdl2:render-fill-rect renderer (sdl2:make-rect (floor *x*) (floor *y*) 100 100)))
 
 (defun mainloop (win renderer)
+  (setf *prev-time* (sdl2:get-ticks))
   (sdl2:with-event-loop (:method :poll)
     (:keydown
      (:keysym keysym)
@@ -40,11 +53,10 @@
      (handle-key keysym :up))
     (:idle
      ()
-     (update-world)
-     (sdl2:set-render-draw-color renderer 0 0 0 255)
-     (sdl2:render-clear renderer)
-     (sdl2:set-render-draw-color renderer 100 200 50 255)
-     (sdl2:render-fill-rect renderer (sdl2:make-rect *x* *y* 100 100))
+     (setf *current-time* (sdl2:get-ticks))
+     (update-world (* (- *current-time* *prev-time*) 0.001))
+     (setf *prev-time* (sdl2:get-ticks))
+     (render-all renderer)
      (sdl2:render-present renderer)
      (sdl2:delay 33))
     (:quit () t)))
